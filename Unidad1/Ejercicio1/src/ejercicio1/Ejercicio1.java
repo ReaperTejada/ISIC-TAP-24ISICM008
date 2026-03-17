0f6f2218fad902eec7e95d73e2eeff88008ccbc7
Tejada <tejada.perez.cristian@gmail.com>
2026-03-16 21:32:00 -0600

    Agregar ejercicio 1 a unidad1

---
+++ Unidad1/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,27 @@
+
+package ejercicio1;
+
+import javax.swing.JFrame;
+
+public class Ejercicio1 extends JFrame {
+    public Ejercicio1(){
+        setTitle("Mi 1ra ventana-TAP");
+        setSize(600,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        
+        new Ejercicio1();
+    }
+}
+   
+
+
+
+
+    
+
+    
